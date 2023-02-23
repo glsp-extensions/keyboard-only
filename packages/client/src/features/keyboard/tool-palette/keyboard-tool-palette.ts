e50272e059c14ee1147c062cddc869b47b4c7414
Aylin Sarioğlu <aylin.sarioglu@outlook.at>
2023-02-23 21:24:53 +0100

    move functionality of keyboard tool palette to new module

--- packages/client/src/features/keyboard/tool-palette/keyboard-tool-palette.ts
+++ packages/client/src/features/keyboard/tool-palette/keyboard-tool-palette.ts
@@ -13,28 +13,14 @@
  *
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  ********************************************************************************/
-import { Action, PaletteItem, RequestContextActions, RequestMarkersAction, SetContextActions } from '@eclipse-glsp/protocol';
-import { inject, injectable, postConstruct } from 'inversify';
-import {
-    AbstractUIExtension,
-    EnableDefaultToolsAction,
-    EnableToolsAction,
-    IActionHandler,
-    ICommand,
-    IToolManager,
-    SetUIExtensionVisibilityAction,
-    SModelRoot,
-    TYPES
-} from 'sprotty';
-import { codiconCSSClasses } from 'sprotty/lib/utils/codicon';
+import { PaletteItem, RequestMarkersAction } from '@eclipse-glsp/protocol';
+import { injectable } from 'inversify';
+import { EnableDefaultToolsAction } from 'sprotty';
 import { KeyCode, matchesKeystroke } from 'sprotty/lib/utils/keyboard';
-
-import { GLSPActionDispatcher } from '../../../base/action-dispatcher';
-import { EditModeListener, EditorContextService } from '../../../base/editor-context-service';
 import { MouseDeleteTool } from '../../tools/delete-tool';
 import { MarqueeMouseTool } from '../../tools/marquee-mouse-tool';
+import { createIcon, changeCodiconClass, createToolGroup, ToolPalette, compare } from '../../../features/tool-palette/tool-palette';
 
-const CLICKED_CSS_CLASS = 'clicked';
 const SEARCH_ICON_ID = 'search';
 const PALETTE_ICON_ID = 'symbol-color';
 const CHEVRON_DOWN_ICON_ID = 'chevron-right';
@@ -75,40 +61,13 @@ const AVAILABLE_KEYS: KeyCode[] = [
 
 const HEADER_TOOL_KEYS: KeyCode[] = [SELECTION_TOOL_KEY, DELETION_TOOL_KEY, MARQUEE_TOOL_KEY, VALIDATION_TOOL_KEY, SEARCH_TOOL_KEY];
 
-export interface EnableToolPaletteAction extends Action {
-    kind: typeof EnableToolPaletteAction.KIND;
-}
-
-export namespace EnableToolPaletteAction {
-    export const KIND = 'enableToolPalette';
-
-    export function is(object: any): object is EnableToolPaletteAction {
-        return Action.hasKind(object, KIND);
-    }
-
-    export function create(): EnableToolPaletteAction {
-        return { kind: KIND };
-    }
-}
 @injectable()
-export class KeyboardToolPalette extends AbstractUIExtension implements IActionHandler, EditModeListener {
-    static readonly ID = 'tool-palette';
-
-    @inject(TYPES.IActionDispatcher) protected readonly actionDispatcher: GLSPActionDispatcher;
-    @inject(TYPES.IToolManager) protected readonly toolManager: IToolManager;
-    @inject(EditorContextService) protected readonly editorContext: EditorContextService;
-
-    protected paletteItems: PaletteItem[];
-    protected paletteItemsCopy: PaletteItem[] = [];
-    protected bodyDiv?: HTMLElement;
-    protected lastActivebutton?: HTMLElement;
-    protected defaultToolsButton: HTMLElement;
-
+export class KeyboardToolPalette extends ToolPalette {
     protected deleteToolButton: HTMLElement;
     protected marqueeToolButton: HTMLElement;
     protected validateToolButton: HTMLElement;
     protected searchToolButton: HTMLElement;
-    protected searchField: HTMLInputElement;
+
     protected keyboardIndexButtonMapping = new Map<number, HTMLElement>();
     protected headerToolsButtonMapping = new Map<number, HTMLElement>();
     protected get interactablePaletteItems(): PaletteItem[] {
@@ -118,28 +77,7 @@ export class KeyboardToolPalette extends AbstractUIExtension implements IActionH
             .reduce((acc, val) => acc.concat(val), []);
     }
 
-    modelRootId: string;
-
-    id(): string {
-        return KeyboardToolPalette.ID;
-    }
-    containerClass(): string {
-        return KeyboardToolPalette.ID;
-    }
-
-    @postConstruct()
-    postConstruct(): void {
-        this.editorContext.register(this);
-    }
-
-    override initialize(): boolean {
-        if (!this.paletteItems) {
-            return false;
-        }
-        return super.initialize();
-    }
-
-    protected initializeContents(_containerElement: HTMLElement): void {
+    protected override initializeContents(_containerElement: HTMLElement): void {
         this.containerElement.tabIndex = 20;
         this.createHeader();
         this.createBody();
@@ -152,12 +90,7 @@ export class KeyboardToolPalette extends AbstractUIExtension implements IActionH
         };
     }
 
-    protected override onBeforeShow(_containerElement: HTMLElement, root: Readonly<SModelRoot>): void {
-        this.modelRootId = root.id;
-        this.containerElement.style.maxHeight = PALETTE_HEIGHT;
-    }
-
-    protected addMinimizePaletteButton(): void {
+    protected override addMinimizePaletteButton(): void {
         const baseDiv = document.getElementById(this.options.baseDiv);
         const minPaletteDiv = document.createElement('div');
         minPaletteDiv.classList.add('minimize-palette-button');
@@ -182,19 +115,7 @@ export class KeyboardToolPalette extends AbstractUIExtension implements IActionH
         }
     }
 
-    protected updateMinimizePaletteButtonTooltip(button: HTMLDivElement): void {
-        if (this.isPaletteMaximized()) {
-            button.title = 'Minimize palette';
-        } else {
-            button.title = 'Maximize palette';
-        }
-    }
-
-    protected isPaletteMaximized(): boolean {
-        return this.containerElement && this.containerElement.style.maxHeight !== '0px';
-    }
-
-    protected createBody(): void {
+    protected override createBody(): void {
         const bodyDiv = document.createElement('div');
         bodyDiv.classList.add('palette-body');
         const tabIndex = 21;
@@ -205,14 +126,14 @@ export class KeyboardToolPalette extends AbstractUIExtension implements IActionH
             if (item.children) {
                 const group = createToolGroup(item);
                 item.children.sort(compare).forEach(child => {
-                    const button = this.createToolButton(child, tabIndex, toolButtonCounter);
+                    const button = this.createKeyboardToolButton(child, tabIndex, toolButtonCounter);
                     group.appendChild(button);
                     this.keyboardIndexButtonMapping.set(toolButtonCounter, button);
                     toolButtonCounter++;
                 });
                 bodyDiv.appendChild(group);
             } else {
-                const button = this.createToolButton(item, tabIndex, toolButtonCounter);
+                const button = this.createKeyboardToolButton(item, tabIndex, toolButtonCounter);
                 bodyDiv.appendChild(button);
                 this.keyboardIndexButtonMapping.set(toolButtonCounter, button);
                 toolButtonCounter++;
@@ -233,17 +154,7 @@ export class KeyboardToolPalette extends AbstractUIExtension implements IActionH
         this.bodyDiv = bodyDiv;
     }
 
-    protected createHeader(): void {
-        this.addMinimizePaletteButton();
-        const headerCompartment = document.createElement('div');
-        headerCompartment.classList.add('palette-header');
-        headerCompartment.append(this.createHeaderTitle());
-        headerCompartment.appendChild(this.createHeaderTools());
-        headerCompartment.appendChild((this.searchField = this.createHeaderSearchField()));
-        this.containerElement.appendChild(headerCompartment);
-    }
-
-    private createHeaderTools(): HTMLElement {
+    protected override createHeaderTools(): HTMLElement {
         this.headerToolsButtonMapping.clear();
 
         const headerTools = document.createElement('div');
@@ -273,7 +184,7 @@ export class KeyboardToolPalette extends AbstractUIExtension implements IActionH
         return headerTools;
     }
 
-    protected createDefaultToolButton(): HTMLElement {
+    protected override createDefaultToolButton(): HTMLElement {
         const button = createIcon('inspect');
         button.id = 'btn_default_tools';
         button.title = 'Enable selection tool';
@@ -283,7 +194,7 @@ export class KeyboardToolPalette extends AbstractUIExtension implements IActionH
         return button;
     }
 
-    protected createMouseDeleteToolButton(): HTMLElement {
+    protected override createMouseDeleteToolButton(): HTMLElement {
         const deleteToolButton = createIcon('chrome-close');
         deleteToolButton.title = 'Enable deletion tool';
         deleteToolButton.onclick = this.onClickStaticToolButton(deleteToolButton, MouseDeleteTool.ID);
@@ -292,7 +203,7 @@ export class KeyboardToolPalette extends AbstractUIExtension implements IActionH
         return deleteToolButton;
     }
 
-    protected createMarqueeToolButton(): HTMLElement {
+    protected override createMarqueeToolButton(): HTMLElement {
         const marqueeToolButton = createIcon('screen-full');
         marqueeToolButton.title = 'Enable marquee tool';
         marqueeToolButton.onclick = this.onClickStaticToolButton(marqueeToolButton, MarqueeMouseTool.ID);
@@ -301,7 +212,7 @@ export class KeyboardToolPalette extends AbstractUIExtension implements IActionH
         return marqueeToolButton;
     }
 
-    protected createValidateButton(): HTMLElement {
+    protected override createValidateButton(): HTMLElement {
         const validateToolButton = createIcon('pass');
         validateToolButton.title = 'Validate model';
         validateToolButton.onclick = _event => {
@@ -313,7 +224,7 @@ export class KeyboardToolPalette extends AbstractUIExtension implements IActionH
         return validateToolButton;
     }
 
-    protected createSearchButton(): HTMLElement {
+    protected override createSearchButton(): HTMLElement {
         const searchIcon = createIcon(SEARCH_ICON_ID);
         searchIcon.onclick = _ev => {
             const searchField = document.getElementById(this.containerElement.id + '_search_field');
@@ -333,7 +244,7 @@ export class KeyboardToolPalette extends AbstractUIExtension implements IActionH
         return searchIcon;
     }
 
-    protected createHeaderSearchField(): HTMLInputElement {
+    protected override createHeaderSearchField(): HTMLInputElement {
         const searchField = document.createElement('input');
         searchField.classList.add('search-input');
         searchField.tabIndex = 21;
@@ -357,14 +268,6 @@ export class KeyboardToolPalette extends AbstractUIExtension implements IActionH
         return searchField;
     }
 
-    protected createHeaderTitle(): HTMLElement {
-        const header = document.createElement('div');
-        header.classList.add('header-icon');
-        header.appendChild(createIcon(PALETTE_ICON_ID));
-        header.insertAdjacentText('beforeend', 'Palette');
-        return header;
-    }
-
     protected createKeyboardShotcut(keyShortcut: KeyCode): HTMLElement {
         const hint = document.createElement('div');
         hint.classList.add('key-shortcut');
@@ -379,7 +282,7 @@ export class KeyboardToolPalette extends AbstractUIExtension implements IActionH
         return hint;
     }
 
-    protected createToolButton(item: PaletteItem, tabIndex: number, buttonIndex: number): HTMLElement {
+    protected createKeyboardToolButton(item: PaletteItem, tabIndex: number, buttonIndex: number): HTMLElement {
         const button = document.createElement('div');
         // add keyboard index
         if (buttonIndex < AVAILABLE_KEYS.length) {
@@ -415,84 +318,12 @@ export class KeyboardToolPalette extends AbstractUIExtension implements IActionH
         return button;
     }
 
-    protected onClickCreateToolButton(button: HTMLElement, item: PaletteItem) {
-        return (_ev: MouseEvent) => {
-            if (!this.editorContext.isReadonly) {
-                this.actionDispatcher.dispatchAll(item.actions);
-                this.changeActiveButton(button);
-                button.focus();
-            }
-        };
-    }
-
-    protected onClickStaticToolButton(button: HTMLElement, toolId?: string) {
-        return (_ev: MouseEvent) => {
-            if (!this.editorContext.isReadonly) {
-                const action = toolId ? EnableToolsAction.create([toolId]) : EnableDefaultToolsAction.create();
-                this.actionDispatcher.dispatch(action);
-                this.changeActiveButton(button);
-                button.focus();
-            }
-        };
-    }
-
-    changeActiveButton(button?: HTMLElement): void {
-        if (this.lastActivebutton) {
-            this.lastActivebutton.classList.remove(CLICKED_CSS_CLASS);
-        }
-        if (button) {
-            button.classList.add(CLICKED_CSS_CLASS);
-            this.lastActivebutton = button;
-        } else {
-            this.defaultToolsButton.classList.add(CLICKED_CSS_CLASS);
-            this.lastActivebutton = this.defaultToolsButton;
-        }
-    }
-
-    handle(action: Action): ICommand | Action | void {
-        if (action.kind === EnableToolPaletteAction.KIND) {
-            const requestAction = RequestContextActions.create({
-                contextId: KeyboardToolPalette.ID,
-                editorContext: {
-                    selectedElementIds: []
-                }
-            });
-            this.actionDispatcher.requestUntil(requestAction).then(response => {
-                if (SetContextActions.is(response)) {
-                    this.paletteItems = response.actions.map(e => e as PaletteItem);
-                    this.actionDispatcher.dispatch(
-                        SetUIExtensionVisibilityAction.create({
-                            extensionId: KeyboardToolPalette.ID,
-                            visible: !this.editorContext.isReadonly
-                        })
-                    );
-                }
-            });
-        } else if (action.kind === EnableDefaultToolsAction.KIND) {
-            this.changeActiveButton();
-            this.restoreFocus();
-        }
-    }
-
-    editModeChanged(_oldValue: string, _newValue: string): void {
-        this.actionDispatcher.dispatch(
-            SetUIExtensionVisibilityAction.create({ extensionId: KeyboardToolPalette.ID, visible: !this.editorContext.isReadonly })
-        );
-    }
-
     protected clickToolOnEnter(event: KeyboardEvent, button: HTMLElement, item: PaletteItem): void {
         if (matchesKeystroke(event, 'Enter')) {
             if (!this.editorContext.isReadonly) {
                 this.actionDispatcher.dispatchAll(item.actions);
                 this.changeActiveButton(button);
             }
-        }
-    }
-
-    protected clearOnEscape(event: KeyboardEvent): void {
-        if (matchesKeystroke(event, 'Escape')) {
-            this.searchField.value = '';
-            this.requestFilterUpdate('');
         }
     }
 
@@ -543,7 +374,7 @@ export class KeyboardToolPalette extends AbstractUIExtension implements IActionH
         }
     }
 
-    protected clearToolOnEscape(event: KeyboardEvent): void {
+    protected override clearToolOnEscape(event: KeyboardEvent): void {
         if (matchesKeystroke(event, 'Escape')) {
             if (event.target instanceof HTMLElement) {
                 event.target.blur();
@@ -551,85 +382,4 @@ export class KeyboardToolPalette extends AbstractUIExtension implements IActionH
             this.actionDispatcher.dispatch(EnableDefaultToolsAction.create());
         }
     }
-
-    protected handleSetContextActions(action: SetContextActions): void {
-        this.paletteItems = action.actions.map(e => e as PaletteItem);
-        this.createBody();
-    }
-
-    protected requestFilterUpdate(filter: string): void {
-        // Initialize the copy if it's empty
-        if (this.paletteItemsCopy.length === 0) {
-            // Creating deep copy
-            this.paletteItemsCopy = JSON.parse(JSON.stringify(this.paletteItems));
-        }
-
-        // Reset the paletteItems before searching
-        this.paletteItems = JSON.parse(JSON.stringify(this.paletteItemsCopy));
-        // Filter the entries
-        const filteredPaletteItems: PaletteItem[] = [];
-        for (const itemGroup of this.paletteItems) {
-            if (itemGroup.children) {
-                // Fetch the labels according to the filter
-                const matchingChildren = itemGroup.children.filter(child => child.label.toLowerCase().includes(filter.toLowerCase()));
-                // Add the itemgroup containing the correct entries
-                if (matchingChildren.length > 0) {
-                    // Clear existing children
-                    itemGroup.children.splice(0, itemGroup.children.length);
-                    // Push the matching children
-                    matchingChildren.forEach(child => itemGroup.children!.push(child));
-                    filteredPaletteItems.push(itemGroup);
-                }
-            }
-        }
-        this.paletteItems = filteredPaletteItems;
-        this.createBody();
-    }
-}
-
-export function compare(a: PaletteItem, b: PaletteItem): number {
-    const sortStringBased = a.sortString.localeCompare(b.sortString);
-    if (sortStringBased !== 0) {
-        return sortStringBased;
-    }
-    return a.label.localeCompare(b.label);
-}
-
-export function createIcon(codiconId: string): HTMLElement {
-    const icon = document.createElement('i');
-    icon.classList.add(...codiconCSSClasses(codiconId));
-    return icon;
-}
-
-export function createToolGroup(item: PaletteItem): HTMLElement {
-    const group = document.createElement('div');
-    group.classList.add('tool-group');
-    group.id = item.id;
-    const header = document.createElement('div');
-    header.classList.add('group-header');
-    if (item.icon) {
-        header.appendChild(createIcon(item.icon));
-    }
-    header.insertAdjacentText('beforeend', item.label);
-    header.ondblclick = _ev => {
-        const css = 'collapsed';
-        changeCSSClass(group, css);
-        Array.from(group.children).forEach(child => changeCSSClass(child, css));
-        window!.getSelection()!.removeAllRanges();
-    };
-
-    group.appendChild(header);
-    return group;
-}
-
-export function changeCSSClass(element: Element, css: string): void {
-    // eslint-disable-next-line chai-friendly/no-unused-expressions
-    element.classList.contains(css) ? element.classList.remove(css) : element.classList.add(css);
-}
-
-export function changeCodiconClass(element: Element, codiconId: string): void {
-    // eslint-disable-next-line chai-friendly/no-unused-expressions
-    element.classList.contains(codiconCSSClasses(codiconId)[1])
-        ? element.classList.remove(codiconCSSClasses(codiconId)[1])
-        : element.classList.add(codiconCSSClasses(codiconId)[1]);
 }
